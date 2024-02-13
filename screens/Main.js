ea18efe5c3fb0c4583bc2b2823db34eeed37bd0b
minaz99 <hanymina43@gmail.com>
2024-02-13 04:37:42 +0100

    feat: added question option circle component

--- screens/Main.js
+++ screens/Main.js
@@ -1,50 +1,86 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
-import Card from "../Components/Card";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts, Inter_600SemiBold } from "@expo-google-fonts/inter";
+import QuestionOptionsCircle from "../Components/QuestionOptionsCircle";
+
 const Main = () => {
+  let [fontsLoaded, fontError] = useFonts({
+    Inter_600SemiBold,
+  });
+
+  /*if (!fontsLoaded && !fontError) {
+    return null;
+  }*/
+  const styles = StyleSheet.create({
+    title: {
+      fontFamily: "Inter_600SemiBold",
+      fontSize: 20,
+      letterSpacing: 5,
+    },
+    question: {
+      fontFamily: "Inter_600SemiBold",
+      fontSize: 11,
+      letterSpacing: 3,
+    },
+  });
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, []);
   const images = {
     burger: require("../src/burger.png"),
+    sunset: require("../src/sunset-.png"),
+    sunrise: require("../src/sunrise.png"),
+    chocolate: require("../src/chocolate.png"),
+    coffee: require("../src/coffee-beans.png"),
+    comedy: require("../src/comedy.png"),
+    flight: require("../src/flight.png"),
+    landscape: require("../src/landscape.png"),
     pizza: require("../src/pizza.png"),
+    rain: require("../src/rain.png"),
+    scream: require("../src/scream.png"),
+    tea: require("../src/tea-bag.png"),
+    train: require("../src/train.png"),
+    vacation: require("../src/vacations.png"),
   };
   return (
-    <SafeAreaView style={{ backgroundColor: "#92BDB5" }} className="h-full ">
-      <Text className="p-12 text-center font-bold text-xl tracking-widest text-white">
-        Do you know your friends
-      </Text>
-
-      <View className="flex-row space-x-6 mx-auto">
-        <Card color="#9F92BD" option={"Burger"} image={images.burger} />
-        <Text className="font-bold  m-auto pr-4 text-white text-lg tracking-widest">
-          OR
+    <LinearGradient colors={["#df89b5", "#bfd9fe"]}>
+      <SafeAreaView className="h-full">
+        <Text
+          style={styles.title}
+          numberOfLines={1}
+          className=" my-6 underline  text-center text-slate-600"
+        >
+          Do you know your friends
         </Text>
-        <Card color="#BD929A" option={"Pizza"} image={images.pizza} />
-      </View>
-      <View className="space-y-3 my-24 mx-auto">
-        <TouchableOpacity
-          style={{ backgroundColor: "#506864" }}
-          className="rounded-lg p-6"
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Text className="text-white text-md tracking-widest font-bold text-center">
-            PLAY
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ backgroundColor: "#AFBD92" }}
-          className="rounded-lg p-6 "
-        >
-          <Text className="text-white text-md tracking-widest font-bold text-center">
-            GAME RULES
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+
+        <QuestionOptionsCircle style={styles.question} images={images} />
+        <View className="m-auto space-y-3 mx-auto">
+          <TouchableOpacity
+            style={{ backgroundColor: "#506864" }}
+            className="rounded-lg p-6"
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text className="text-white text-md tracking-widest font-bold text-center">
+              PLAY
+            </Text>
+          </TouchableOpacity>
+          <LinearGradient
+            className="rounded-full"
+            colors={["#ff758c", "#ff7eb3"]}
+          >
+            <TouchableOpacity className=" p-6">
+              <Text className="text-white  text-md tracking-widest font-bold text-center">
+                GAME RULES
+              </Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
