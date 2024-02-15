3b4710ab6b665de0f52e66485ec712da83eb7117
minaz99 <hanymina43@gmail.com>
2024-02-15 16:07:09 +0100

    fix: moved imported images to a new component Images

--- screens/Main.js
+++ screens/Main.js
@@ -91,22 +91,7 @@ const Main = ({ navigation }) => {
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, []);
-  const images = {
-    burger: require("../src/burger.png"),
-    sunset: require("../src/sunset-.png"),
-    sunrise: require("../src/sunrise.png"),
-    chocolate: require("../src/chocolate.png"),
-    coffee: require("../src/coffee-beans.png"),
-    comedy: require("../src/comedy.png"),
-    flight: require("../src/flight.png"),
-    landscape: require("../src/landscape.png"),
-    pizza: require("../src/pizza.png"),
-    rain: require("../src/rain.png"),
-    scream: require("../src/scream.png"),
-    tea: require("../src/tea-bag.png"),
-    train: require("../src/train.png"),
-    vacation: require("../src/vacations.png"),
-  };
+
   return (
     <LinearGradient colors={["#df89b5", "#bfd9fe"]}>
       <SafeAreaView className="h-full  ">
@@ -120,7 +105,7 @@ const Main = ({ navigation }) => {
           </Animated.Text>
 
           <View>
-            <QuestionOptionsCircle style={styles.question} images={images} />
+            <QuestionOptionsCircle style={styles.question} />
           </View>
         </View>
         <View className="m-auto space-y-6 mx-auto">
