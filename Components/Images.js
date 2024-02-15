2fb0db387ff5ab7f817565324d186a78ff5a6cce
minaz99 <hanymina43@gmail.com>
2024-02-15 16:06:39 +0100

    feat: new images component that contains all images

---
+++ Components/Images.js
@@ -0,0 +1,18 @@
+const Images = {
+  burger: require("../src/burger.png"),
+  sunset: require("../src/sunset-.png"),
+  sunrise: require("../src/sunrise.png"),
+  chocolate: require("../src/chocolate.png"),
+  coffee: require("../src/coffee-beans.png"),
+  comedy: require("../src/comedy.png"),
+  flight: require("../src/flight.png"),
+  landscape: require("../src/landscape.png"),
+  pizza: require("../src/pizza.png"),
+  rain: require("../src/rain.png"),
+  scream: require("../src/scream.png"),
+  tea: require("../src/tea-bag.png"),
+  train: require("../src/train.png"),
+  vacation: require("../src/vacations.png"),
+};
+
+export default Images;
