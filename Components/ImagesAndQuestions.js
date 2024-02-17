8f4d06abaad03353851e5896feb158a732bfe4e6
minaz99 <hanymina43@gmail.com>
2024-02-17 21:30:53 +0100

    feat: created image and questions objects

---
+++ Components/ImagesAndQuestions.js
@@ -0,0 +1,54 @@
+import Images from "./Images";
+const ImagesAndQuestions = {
+  view: {
+    img1: Images.sunset,
+    img2: Images.sunrise,
+    question: "Which do you enjoy watching sunsets or sunrises",
+  },
+  travel: {
+    img1: Images.flight,
+    img2: Images.train,
+    question: "Which way of travelling do you prefer",
+  },
+
+  movie: {
+    img1: Images.scream,
+    img2: Images.comedy,
+    question: "What type of movies do you like to watch in a movie night",
+  },
+  weather: {
+    img1: Images.rain,
+    img2: Images.snow,
+    question: "what kind of weather would you want to have all the time",
+  },
+  icecream: {
+    img1: Images.chocolate,
+    img2: Images.vanilla,
+    question: "If you were to be an icecream which flavor would you be",
+  },
+  speed: {
+    img1: Images.turtle,
+    img2: Images.snail,
+    question:
+      "would you rather fly as slow as a snail or run as slow as a turtle",
+  },
+  touching: {
+    img1: Images.lion,
+    img2: Images.snake,
+    question: "Which would you feel more safe touching, a lion or a snake",
+  },
+  gift: {
+    img1: Images.gift,
+    img2: Images.loveletter,
+    question:
+      "Would you like to receive a handwritten love letter or a surprise gift",
+  },
+  relaxing: {
+    img1: Images.vacation,
+    img2: Images.sightseeing,
+    question:
+      "Do you prefer relaxing places with beach and sun or places with adventurous explorations",
+  },
+};
+
+export default ImagesAndQuestions;
