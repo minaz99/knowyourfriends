5267afb111c0f1739bf653ba3b6d6a7f594c16e8
minaz99 <hanymina43@gmail.com>
2024-02-17 22:50:03 +0100

    feat: new styles component with title, question, headlines and inlines styles added

---
+++ styles/Styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const Styles = StyleSheet.create({
+  title: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 20,
+    letterSpacing: 5,
+  },
+  question: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 10,
+    letterSpacing: 3,
+  },
+  headlines: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 17,
+    letterSpacing: 4,
+  },
+  inlines: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 15,
+    letterSpacing: 3,
+  },
+});
+
+//export default Styles;
