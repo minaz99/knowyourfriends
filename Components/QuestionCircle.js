34024bc44d1de7661ff4ea0bd174ae388091eed0
minaz99 <hanymina43@gmail.com>
2024-02-17 21:32:23 +0100

    feat: addded animation to text and images seperately

--- Components/QuestionCircle.js
+++ Components/QuestionCircle.js
@@ -1,34 +1,66 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text, Image, Animated, Easing } from "react-native";
+import React, { useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 const QuestionCircle = (props) => {
+  let yValue = new Animated.Value(-120);
+  let opacityAnimation = new Animated.Value(0);
+  startAnimation = () => {
+    Animated.timing(yValue, {
+      toValue: 0,
+      duration: 2500,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+    Animated.timing(opacityAnimation, {
+      toValue: 1,
+      duration: 2500,
+      useNativeDriver: true,
+    }).start();
+  };
+  const animatedStyles = {
+    transform: [
+      {
+        translateY: yValue,
+      },
+    ],
+    opacity: opacityAnimation,
+  };
+  startAnimation();
+  useEffect(() => {
+    startAnimation();
+  }, [props.data]);
   return (
-    <LinearGradient
-      className="rounded-full mx-auto w-56 h-56"
-      colors={["#a8edea", "#fed6e3"]}
+    <Animated.View
+      style={[props.opacityStyle, props.animatedStyles]}
+      className=""
     >
-      <View className="mx-auto space-y-4 m-auto p-3.5">
-        <Text
-          style={props.style}
-          className="text-slate-600 p-2 capitalize font-medium"
-        >
-          {props.question} ....
-        </Text>
-        <View className="space-x-2 items-center mx-auto  flex-row">
-          <Image
-            source={props.image1}
-            className="rounded-full h-16 w-16"
-          ></Image>
-          <Text style={props.style} className="text-slate-600 text-lg">
-            OR
+      <LinearGradient
+        className="rounded-full flex items-center justify-center p-2 text-center w-56 h-56  m-auto"
+        colors={["#a8edea", "#fed6e3"]}
+      >
+        <Animated.View style={animatedStyles}>
+          <Text
+            style={props.style}
+            className="text-slate-600 p-2 capitalize font-medium"
+          >
+            {props.data.question} ....
           </Text>
-          <Image
-            source={props.image2}
-            className="h-16 w-16 rounded-full"
-          ></Image>
-        </View>
-      </View>
-    </LinearGradient>
+          <View className="space-x-2 items-center mx-auto  flex-row">
+            <Image
+              source={props.data.img1}
+              className="rounded-full h-16 w-16"
+            ></Image>
+            <Text style={props.style} className="text-slate-600 text-lg">
+              OR
+            </Text>
+            <Image
+              source={props.data.img2}
+              className="h-16 w-16 rounded-full"
+            ></Image>
+          </View>
+        </Animated.View>
+      </LinearGradient>
+    </Animated.View>
   );
 };
 
