178690e1a5ba471c4df60f01eb9b37c3c9a4ce8f
minaz99 <hanymina43@gmail.com>
2024-02-19 20:39:12 +0100

    feat: added animations + new design for join

--- screens/Join.js
+++ screens/Join.js
@@ -4,19 +4,66 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  Animated,
+  Easing,
 } from "react-native";
-import React, { useLayoutEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import axios from "axios";
 import io from "socket.io-client";
-const Join = () => {
-  const navigation = useNavigation();
+import { LinearGradient } from "expo-linear-gradient";
+import { ArrowLeftCircleIcon } from "react-native-heroicons/outline";
+import { Styles } from "../styles/Styles";
+const Join = ({ navigation }) => {
+  let yValueCreateJoin = new Animated.Value(-100);
+  let yValue = new Animated.Value(-120);
+  let opacityAnimation = new Animated.Value(0);
+  startAnimation = () => {
+    Animated.timing(yValue, {
+      toValue: 0,
+      duration: 2000,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+    Animated.timing(opacityAnimation, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: true,
+      //easing: Easing.bounce,
+    }).start();
+    Animated.timing(yValueCreateJoin, {
+      toValue: 0,
+      duration: 1500,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+  };
+  const AS = {
+    animatedStyles: {
+      transform: [
+        {
+          translateY: yValue,
+        },
+      ],
+    },
+    animatedStylesBtns: {
+      transform: [
+        {
+          translateY: yValueCreateJoin,
+        },
+      ],
+    },
+    opacityStyle: { opacity: opacityAnimation },
+  };
+  useEffect(() => {
+    startAnimation();
+  }, []);
+
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   });
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [gameID, setGameID] = useState(-1);
+  const [gameID, setGameID] = useState("");
   const joinGame = async () => {
     await axios
       .post("http://192.168.43.135:3003/game/join", {
@@ -37,78 +84,109 @@ const Join = () => {
   };
 
   return (
-    <SafeAreaView style={{ backgroundColor: "#92BDB5" }} className="h-full ">
-      <Text className="tracking-widest text-lg font-bold text-center mt-24 text-white">
-        JOIN GAME
-      </Text>
-      <View className="space-y-4 items-center  p-8">
-        <View>
-          <Text
-            style={{ color: "#DEEBE9" }}
-            className="text-white text-center font-bold text-lg tracking-widest"
-          >
-            Username
-          </Text>
-          <TextInput
-            placeholder="nickname"
-            style={{
-              backgroundColor: "#FED7AA",
-              borderRadius: 20,
-              height: 40,
-            }}
-            className="text-lg w-44 text-center"
-            onChangeText={setUsername}
-          ></TextInput>
-        </View>
+    <LinearGradient colors={["#df89b5", "#bfd9fe"]}>
+      <SafeAreaView className="h-full ">
+        <Animated.View style={[AS.opacityStyle, AS.animatedStyles]}>
+          <TouchableOpacity onPress={() => navigation.goBack()} className="p-6">
+            <ArrowLeftCircleIcon color={"#475569"} height={32} width={32} />
+          </TouchableOpacity>
+        </Animated.View>
+        <Animated.Text
+          style={[Styles.title, AS.animatedStyles, AS.opacityStyle]}
+          numberOfLines={1}
+          className=" text-center  text-slate-600"
+        >
+          JOIN GAME
+        </Animated.Text>
 
-        <View>
-          <Text
-            style={{ color: "#DEEBE9" }}
-            className="text-white text-center font-bold text-lg tracking-widest"
+        <Animated.View
+          style={[AS.opacityAnimation, AS.animatedStyles]}
+          className="space-y-4 items-center  p-8"
+        >
+          <View className="space-y-2">
+            <Text
+              style={[Styles.headlines]}
+              className="text-white text-center font-bold text-lg tracking-widest"
+            >
+              Username
+            </Text>
+            <View className="m-auto overflow-x-scroll">
+              <TextInput
+                placeholderTextColor={"#0f172a"}
+                placeholder="Nickname"
+                style={[
+                  username !== ""
+                    ? Styles.filledInputText
+                    : Styles.emptyInputText,
+                  Styles.headlines,
+                ]}
+                className="text-lg w-44  text-center text-slate-600 "
+                onChangeText={setUsername}
+              ></TextInput>
+            </View>
+          </View>
+          <View className="space-y-2">
+            <Text
+              style={[Styles.headlines]}
+              className="text-white text-center font-bold text-lg tracking-widest"
+            >
+              Game ID
+            </Text>
+            <View className="m-auto">
+              <TextInput
+                placeholder="123"
+                placeholderTextColor={"#0f172a"}
+                style={[
+                  gameID !== ""
+                    ? Styles.filledInputText
+                    : Styles.emptyInputText,
+                  Styles.headlines,
+                ]}
+                className="text-lg w-44 text-center text-slate-600"
+                onChangeText={setGameID}
+                keyboardType="numeric"
+              ></TextInput>
+            </View>
+          </View>
+          <View className="space-y-2">
+            <Text
+              style={[Styles.headlines]}
+              className="text-white text-center font-bold text-lg tracking-widest"
+            >
+              Game Password
+            </Text>
+            <View className="m-auto">
+              <TextInput
+                placeholderTextColor={"#0f172a"}
+                placeholder="Password"
+                style={[
+                  password !== ""
+                    ? Styles.filledInputText
+                    : Styles.emptyInputText,
+                  Styles.headlines,
+                ]}
+                className="text-lg w-44 text-center text-slate-600"
+                onChangeText={setPassword}
+              ></TextInput>
+            </View>
+          </View>
+        </Animated.View>
+        <Animated.View style={[AS.animatedStylesBtns, AS.opacityStyle]}>
+          <TouchableOpacity
+            style={{ backgroundColor: "#9F92BD" }}
+            className="rounded-3xl w-2/4 p-6 mx-auto "
+            onPress={async () => await joinGame()}
           >
-            Game ID
-          </Text>
-          <TextInput
-            placeholder="123"
-            style={{
-              backgroundColor: "#FED7AA",
-              borderRadius: 20,
-              height: 40,
-            }}
-            className="text-lg w-44 text-center"
-            onChangeText={setGameID}
-            keyboardType="numeric"
-          ></TextInput>
-        </View>
-        <View>
-          <Text
-            style={{ color: "#DEEBE9" }}
-            className="text-white text-center font-bold text-lg tracking-widest"
-          >
-            Game Password
-          </Text>
-          <TextInput
-            placeholder="password"
-            style={{
-              backgroundColor: "#FED7AA",
-              borderRadius: 20,
-              height: 40,
-            }}
-            className="text-lg w-44 text-center"
-            onChangeText={setPassword}
-          ></TextInput>
-        </View>
-      </View>
-      <TouchableOpacity
-        style={{ backgroundColor: "#9F92BD" }}
-        className="rounded-3xl w-2/4 p-6 mx-auto"
-        onPress={async () => await joinGame()}
-      >
-        <Text className="text-white text-md tracking-widest font-bold text-center">
-          JOIN
-        </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+            <Text
+              style={Styles.inlines}
+              className="text-white text-md tracking-widest font-bold text-center"
+            >
+              JOIN
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
