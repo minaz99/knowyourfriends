4db9264078493f115de2a5741c0e69a1a281fdc3
minaz99 <hanymina43@gmail.com>
2024-02-23 19:16:48 +0100

    feat: added linear gradients to username & timer + added  lienar gradients to be used in question components

--- screens/Game.js
+++ screens/Game.js
@@ -1,9 +1,11 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Platform } from "react-native";
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import Scores from "../Components/Scores";
 import Guessing from "../Components/Guessing";
 import Selection from "../Components/Selection";
 import GameFinished from "../Components/GameFinished";
+import { Styles } from "../styles/Styles";
+import { LinearGradient } from "expo-linear-gradient";
 const Game = ({ navigation, route }) => {
   const { gameID, gameDetails, player, socket } = route.params;
   useLayoutEffect(() => {
@@ -11,9 +13,10 @@ const Game = ({ navigation, route }) => {
   });
   const [gameInfo, setGameInfo] = useState(gameDetails);
   const [timer, setTimer] = useState(60);
-  const colorA1 = "#9F92BD";
-  const colorA2 = "#BD929A";
-
+  const colorA1 = "#ef4444";
+  const colorA2 = "#f59e0b";
+  const gradientA1 = ["#1e3c72", "#2a5298"];
+  const gradientA2 = ["#ff758c", "#ff7eb3"];
   useEffect(() => {
     socket.on("start timer", () => {
       if (player.id === gameInfo.gameData.hostid) {
@@ -29,56 +32,86 @@ const Game = ({ navigation, route }) => {
     socket.on("updated", (gameDetails) => setGameInfo(gameDetails));
   }, []);
   return (
-    <SafeAreaView
-      style={{ backgroundColor: "#92BDB5" }}
-      className="h-full flex relative  "
-    >
-      <View className="flex-row mt-8">
-        <Text className="p-4 flex-1 text-slate-600 trackinhg-widest font-bold text-lg">
-          {timer}
-        </Text>
-        <Text className="p-4 text-slate-600 tracking-widest font-bold text-lg">
-          #{player.username}
-        </Text>
-      </View>
-      {gameInfo.gameData.stage === "guessing" ? (
-        <Guessing
-          gameID={gameID}
-          a1Color={colorA1}
-          a2Color={colorA2}
-          gameDetails={gameInfo}
-          playerID={player.id}
-          socket={socket}
-          setTimer={setTimer}
-        />
-      ) : gameInfo.gameData.stage === "selection" ? (
-        <Selection
-          a1Color={colorA1}
-          a2Color={colorA2}
-          gameDetails={gameInfo}
-          gameID={gameID}
-          playerID={player.id}
-          socket={socket}
-        />
-      ) : (
-        <GameFinished
-          scores={gameInfo.scores}
-          round={`${gameInfo.gameData.currentround} / ${gameInfo.gameData.rounds}`}
-        />
-      )}
-      {
-        <View className="inset-x-0 bottom-0 absolute">
-          {gameInfo.gameData.stage !== "game finished" ? (
-            <Scores
-              scores={gameInfo.scores}
-              round={`${gameInfo.gameData.currentround} / ${gameInfo.gameData.rounds}`}
-            />
-          ) : (
-            <View></View>
-          )}
+    <LinearGradient colors={["#df89b5", "#bfd9fe"]}>
+      <SafeAreaView className="h-full flex relativep ">
+        <View
+          style={Platform.OS === "android" ? Styles.GameAnd : ""}
+          className="flex-row justify-between p-4 "
+        >
+          <LinearGradient
+            colors={["#c1dfc4", "#ace0f9"]}
+            className="rounded-full  p-1.5"
+          >
+            <View className="rounded-full m-auto text-center bg-white p-3">
+              <Text
+                style={[Styles.inlinesForDropDown]}
+                className="text-center   text-slate-600"
+              >
+                Time: {timer}
+              </Text>
+            </View>
+          </LinearGradient>
+          <LinearGradient
+            //className="rounded-full"
+            colors={["#c1dfc4", "#ace0f9"]}
+            className="rounded-full  p-1.5"
+          >
+            <View className="rounded-full bg-white p-3">
+              <Text
+                className="text-slate-600"
+                style={
+                  Styles.inlinesForDropDown
+                } /*className=" text-slate-600 tracking-widest font-bold text-lg"*/
+              >
+                #{player.username}
+              </Text>
+            </View>
+          </LinearGradient>
         </View>
-      }
-    </SafeAreaView>
+
+        {gameInfo.gameData.stage === "guessing" ? (
+          <Guessing
+            gameID={gameID}
+            a1Color={colorA1}
+            a2Color={colorA2}
+            gameDetails={gameInfo}
+            playerID={player.id}
+            socket={socket}
+            setTimer={setTimer}
+            gradientA1={gradientA1}
+            gradientA2={gradientA2}
+          />
+        ) : gameInfo.gameData.stage === "selection" ? (
+          <Selection
+            a1Color={colorA1}
+            a2Color={colorA2}
+            gameDetails={gameInfo}
+            gameID={gameID}
+            playerID={player.id}
+            socket={socket}
+            gradientA1={gradientA1}
+            gradientA2={gradientA2}
+          />
+        ) : (
+          <GameFinished
+            scores={gameInfo.scores}
+            round={`${gameInfo.gameData.currentround} / ${gameInfo.gameData.rounds}`}
+          />
+        )}
+        {
+          <View className="inset-x-0 bottom-0 absolute">
+            {gameInfo.gameData.stage !== "game finished" ? (
+              <Scores
+                scores={gameInfo.scores}
+                round={`${gameInfo.gameData.currentround} / ${gameInfo.gameData.rounds}`}
+              />
+            ) : (
+              <View></View>
+            )}
+          </View>
+        }
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
