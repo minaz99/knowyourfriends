81f522570a945a9b71ec282ac34595afde54c6a0
minaz99 <hanymina43@gmail.com>
2024-02-23 19:15:34 +0100

    feat: added new gradient coloring + new background color opacity

--- Components/QuestionGuessing.js
+++ Components/QuestionGuessing.js
@@ -1,15 +1,16 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-
+import { LinearGradient } from "expo-linear-gradient";
+import { Styles } from "../styles/Styles";
 const QuestionGuessing = (props) => {
   const questionFormatter = (question) => {
     let situation = question.split(":")[0];
-    situation = situation.concat(" . . . . . . .");
+    situation = situation.concat(" . . . .");
     const a1 = question.split(":")[1].split("||")[0];
     const a2 = question.split(":")[1].split("||")[1];
     return { situation: situation, a1: a1, a2: a2 };
   };
-
+  //const gradientA2 = ["#ff758c", "#ff7eb3"];
   const chooseAnswer = (answer) => {
     props.socket.emit("guessed", {
       answer: answer,
@@ -20,58 +21,92 @@ const QuestionGuessing = (props) => {
   };
 
   return (
-    <View className="space-y-6 mx-auto">
-      <View className="bg-slate-600 p-2 rounded-2xl">
-        <Text className="text-white tracking-widest text-lg font-medium">
-          {questionFormatter(props.question).situation}
-        </Text>
+    <LinearGradient
+      className="rounded-3xl bg-white/60 p-2  "
+      colors={["#c1dfc4", "#ace0f9"]}
+    >
+      <View className="rounded-3xl bg-white/60 p-2 mx-auto">
+        <View className="p-4">
+          <Text style={Styles.inlines} className="text-slate-600 ">
+            {questionFormatter(props.question).situation}
+          </Text>
+        </View>
+        {props.isGuessing ? (
+          <View className="space-y-2  p-4">
+            <LinearGradient className="p-1.5 rounded-full" colors={props.a1}>
+              <TouchableOpacity
+                onPress={() => chooseAnswer(1)}
+                className="p-2 bg-white rounded-full"
+              >
+                <Text
+                  numberOfLines={2}
+                  adjustsFontSizeToFit={true}
+                  style={Styles.inlines}
+                  className="text-slate-600 text-center "
+                >
+                  {questionFormatter(props.question).a1}
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+
+            {/*<Text className="text-slate-600 text-lg font-bold mx-auto">OR</Text>*/}
+            <LinearGradient className="p-1.5 rounded-full" colors={props.a2}>
+              <TouchableOpacity
+                onPress={() => chooseAnswer(2)}
+                className="p-2 bg-white rounded-full"
+              >
+                <Text
+                  numberOfLines={2}
+                  adjustsFontSizeToFit={true}
+                  style={Styles.inlines}
+                  className="text-slate-600 text-center "
+                >
+                  {questionFormatter(props.question).a2}
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        ) : (
+          <View className="space-y-2 p-4">
+            <LinearGradient className="p-1.5 rounded-full" colors={props.a1}>
+              <TouchableOpacity
+                className="p-2 bg-white rounded-full"
+                //style={{ backgroundColor: props.a2 }}
+                disabled
+              >
+                <Text
+                  numberOfLines={2}
+                  adjustsFontSizeToFit={true}
+                  style={Styles.inlines}
+                  className="text-slate-600 text-center "
+                >
+                  {questionFormatter(props.question).a1}
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+            {/*<Text style={Styles.inlines} className="text-slate-600 mx-auto">
+            OR
+      </Text>*/}
+            <LinearGradient className="p-1.5 rounded-full" colors={props.a2}>
+              <TouchableOpacity
+                className="p-2 bg-white rounded-full"
+                //style={{ backgroundColor: props.a2 }}
+                disabled
+              >
+                <Text
+                  numberOfLines={2}
+                  adjustsFontSizeToFit={true}
+                  style={Styles.inlines}
+                  className="text-slate-600 text-center "
+                >
+                  {questionFormatter(props.question).a2}
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        )}
       </View>
-      {props.isGuessing ? (
-        <View className="space-y-3 p-4">
-          <TouchableOpacity
-            onPress={() => chooseAnswer(1)}
-            className="p-2 rounded-2xl"
-            style={{ backgroundColor: props.a1 }}
-          >
-            <Text className="text-white text-center tracking-widest font-medium text-lg">
-              {questionFormatter(props.question).a1}
-            </Text>
-          </TouchableOpacity>
-          <Text className="text-white text-lg font-bold mx-auto">OR</Text>
-          <TouchableOpacity
-            onPress={() => chooseAnswer(2)}
-            className="p-2 rounded-2xl"
-            style={{ backgroundColor: props.a2 }}
-          >
-            <Text className="text-white text-center tracking-widest font-medium text-lg">
-              {questionFormatter(props.question).a2}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      ) : (
-        <View className="space-y-3 p-4">
-          <TouchableOpacity
-            className="p-2 rounded-2xl"
-            style={{ backgroundColor: props.a1 }}
-            disabled
-          >
-            <Text className="text-white text-center tracking-widest font-medium text-lg">
-              {questionFormatter(props.question).a1}
-            </Text>
-          </TouchableOpacity>
-          <Text className="text-white text-lg font-bold mx-auto">OR</Text>
-          <TouchableOpacity
-            className="p-2 rounded-2xl"
-            style={{ backgroundColor: props.a2 }}
-            disabled
-          >
-            <Text className="text-white text-center tracking-widest font-medium text-lg">
-              {questionFormatter(props.question).a2}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </View>
+    </LinearGradient>
   );
 };
 
