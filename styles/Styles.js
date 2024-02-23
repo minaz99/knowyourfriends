bc39a4427c5cd7b8a51e2454472652f8255713dd
minaz99 <hanymina43@gmail.com>
2024-02-23 02:36:38 +0100

    feat: new styles for IOS and Android

--- styles/Styles.js
+++ styles/Styles.js
@@ -1,28 +1,41 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export const Styles = StyleSheet.create({
   title: {
-    fontFamily: "Inter_600SemiBold",
+    fontFamily: "Inter-SemiBold",
     fontSize: 20,
     letterSpacing: 5,
   },
+  mainStyleAnd: {
+    //margin: 22,
+    paddingTop: 18,
+  },
+  createJoinStyleIOS: {
+    padding: 12,
+  },
+  lobbyIOS: {
+    padding: 8,
+  },
+  lobbyAnd: {
+    padding: 18,
+  },
   question: {
-    fontFamily: "Inter_600SemiBold",
+    fontFamily: "Inter-SemiBold",
     fontSize: 10,
     letterSpacing: 3,
   },
   headlines: {
-    fontFamily: "Inter_600SemiBold",
+    fontFamily: "Inter-SemiBold",
     fontSize: 17,
     letterSpacing: 4,
   },
   inlines: {
-    fontFamily: "Inter_600SemiBold",
+    fontFamily: "Inter-SemiBold",
     fontSize: 15,
     letterSpacing: 3,
   },
   inlinesForDropDown: {
-    fontFamily: "Inter_600SemiBold",
+    fontFamily: "Inter-SemiBold",
     fontSize: 12,
     letterSpacing: 3,
   },
