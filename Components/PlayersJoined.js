9611b25e6a46fe4832e0403b8910417c18ffa9c9
minaz99 <hanymina43@gmail.com>
2024-02-23 02:33:59 +0100

    feat: added linear gradient to component

--- Components/PlayersJoined.js
+++ Components/PlayersJoined.js
@@ -1,21 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, Animated } from "react-native";
+import React, { useEffect } from "react";
+import { Styles } from "../styles/Styles";
+import { LinearGradient } from "expo-linear-gradient";
 const PlayersJoined = (props) => {
   return (
     <View className=" p-6 content-start gap-4 grid-cols-4 flex-row">
       {props.players.length > 0 ? (
         props.players.map((player) => {
           return (
-            <View
+            <LinearGradient
+              colors={["#f3e7e9", "#e3eeff"]}
+              className="rounded-full  w-fit p-2"
               key={player.id}
-              style={{ backgroundColor: "#D9D9D9" }}
-              className="rounded-full w-fit p-2 "
             >
-              <Text className="text-center font-bold text-slate-600">
-                #{player.username}
-              </Text>
-            </View>
+              <View
+                key={player.id}
+                // style={AS.opacityStyle}
+                //className="rounded-full  w-fit p-2 bg-green-100"
+              >
+                <Text
+                  style={Styles.inlines}
+                  className="text-center font-bold text-slate-600"
+                >
+                  #{player.username}
+                </Text>
+              </View>
+            </LinearGradient>
           );
         })
       ) : (
