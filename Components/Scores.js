4171d470ff56055ae319a50bdd958da3d462dcc6
minaz99 <hanymina43@gmail.com>
2024-02-29 23:30:25 +0100

    feat: new design

--- Components/Scores.js
+++ Components/Scores.js
@@ -1,37 +1,47 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Styles } from "../styles/Styles";
 const Scores = (props) => {
   const [scoresClicked, setScoresClicked] = useState(false);
   return scoresClicked ? (
-    <View className="rounded-t-lg bg-white/80 p-4 space-y-3">
+    <View className="rounded-t-lg bg-white p-4 space-y-3">
       <TouchableOpacity onPress={() => setScoresClicked(false)}>
-        <Text className="text-slate-600 font-bold text-lg text-center tracking-widest">
+        <Text
+          style={Styles.headlines}
+          className="text-slate-600 font-bold text-lg text-center tracking-widest"
+        >
           Scores ↓
         </Text>
-        <Text className="text-slate-600 font-medium text-md text-center tracking-widest">
+        <Text
+          style={Styles.inlines}
+          className="text-slate-600 font-medium text-md text-center tracking-widest"
+        >
           Round {props.round}
         </Text>
       </TouchableOpacity>
       {props.scores.map((player) => {
         return (
-          <View
-            key={player.id}
-            className="rounded-2xl flex-row p-2 bg-slate-600"
+          <LinearGradient
+            className="rounded-2xl"
+            colors={["#f794a4", "#fdd6bd"]}
           >
-            <Text className="text-white text-lg tracking-widest font-bold flex-1">
-              #{player.username}
-            </Text>
-            <Text className="text-white text-lg tracking-widest font-bold">
-              {player.score}
-            </Text>
-          </View>
+            <View key={player.id} className="rounded-2xl flex-row p-2 ">
+              <Text style={Styles.inlines} className="text-slate-600  flex-1">
+                #{player.username}
+              </Text>
+              <Text style={Styles.headlines} className="text-slate-600">
+                {player.score}
+              </Text>
+            </View>
+          </LinearGradient>
         );
       })}
     </View>
   ) : (
     <TouchableOpacity
       onPress={() => setScoresClicked(true)}
-      className="bg-white p-4 rounded-t-lg"
+      className="bg-white  items-center  p-4 rounded-t-lg"
     >
       <Text className="text-slate-600 font-bold text-lg text-center tracking-widest">
         Scores ↑
