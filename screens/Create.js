4752e9d46bb6dae590bccc119b85145a700e0c06
minaz99 <hanymina43@gmail.com>
2024-03-06 15:27:38 +0100

    feat: added scrollview and made scroll to end feature on displaying error message

--- screens/Create.js
+++ screens/Create.js
@@ -8,8 +8,9 @@ import {
   Easing,
   Platform,
   LogBox,
+  ScrollView,
 } from "react-native";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import axios from "axios";
 import io from "socket.io-client";
 import { LinearGradient } from "expo-linear-gradient";
@@ -89,204 +90,251 @@ const Create = ({ navigation, route }) => {
   const [rounds, setRounds] = useState("");
   const [password, setPassword] = useState("");
   const [language, setLanguage] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  let errorRef;
+  const allFieldsFilled = () => {
+    if (
+      username.length !== 0 &&
+      players.length !== 0 &&
+      rounds.length !== 0 &&
+      password.length !== 0 &&
+      language.length !== 0
+    ) {
+      if (parseInt(rounds) % parseInt(players) !== 0) {
+        setErrorMsg("Rounds must be divisible by players count");
+        errorRef.scrollToEnd({ animated: true });
+        return false;
+      }
+      setErrorMsg("");
+      return true;
+    } else {
+      setErrorMsg("Make sure you entered all fields");
+      errorRef.scrollToEnd({ animated: true });
+      return false;
+    }
+  };
   const createGame = async () => {
-    await axios
-      .post("http://192.168.43.135:3003/game/create", {
-        username: username,
-        noOfPlayers: players,
-        rounds: rounds,
-        password: password,
-      })
-      .then((resp) => {
-        let socket = io("http://192.168.43.135:3003");
-        socket.emit("join", resp.data.gameID);
-        navigation.navigate("Lobby", {
-          player: resp.data.player,
-          socket,
-          gameID: resp.data.gameID,
+    if (allFieldsFilled()) {
+      await axios
+        .post("http://192.168.43.135:3003/game/create", {
+          username: username,
+          noOfPlayers: players,
+          rounds: rounds,
           password: password,
-          bgMusic: bgMusic,
+        })
+        .then((resp) => {
+          let socket = io("http://192.168.43.135:3003");
+          socket.emit("join", resp.data.gameID);
+          navigation.navigate("Lobby", {
+            player: resp.data.player,
+            socket,
+            gameID: resp.data.gameID,
+            password: password,
+            bgMusic: bgMusic,
+          });
+        })
+        .catch((error) => {
+          setErrorMsg("Network error");
+          errorRef.scrollToEnd({ animated: true });
         });
-      })
-      .catch((error) => console.log(error));
+    }
   };
 
   return (
     <LinearGradient colors={["#df89b5", "#bfd9fe"]}>
       <SafeAreaView className="h-full p-6">
-        <Animated.View style={[AS.opacityStyle, AS.animatedStyles]}>
-          <TouchableOpacity
-            style={Platform.OS === "ios" ? Styles.createJoinStyleIOS : ""}
-            onPress={() => {
-              playSound();
-              navigation.goBack();
-            }}
-          >
-            <ArrowLeftCircleIcon color={"#475569"} height={32} width={32} />
-          </TouchableOpacity>
-        </Animated.View>
-        <Animated.Text
-          style={[Styles.title, AS.animatedStyles, AS.opacityStyle]}
-          numberOfLines={1}
-          className=" text-center  text-slate-600"
+        <ScrollView
+          ref={(ref) => {
+            errorRef = ref;
+          }}
+          showsVerticalScrollIndicator={false}
         >
-          CREATE GAME
-        </Animated.Text>
-
-        <Animated.View
-          style={[AS.opacityAnimation, AS.animatedStyles]}
-          className="space-y-4 items-center  p-8"
-        >
-          <View className="space-y-2">
-            <Text
-              style={[Styles.headlines]}
-              className="text-white text-center font-bold text-lg tracking-widest"
+          <Animated.View style={[AS.opacityStyle, AS.animatedStyles]}>
+            <TouchableOpacity
+              style={Platform.OS === "ios" ? Styles.createJoinStyleIOS : ""}
+              onPress={() => {
+                playSound();
+                navigation.goBack();
+              }}
             >
-              Username
-            </Text>
-            <View className="m-auto overflow-x-scroll">
-              <TextInput
-                onPressIn={playFieldSound}
-                placeholderTextColor={"#0f172a"}
-                placeholder="Nickname"
-                style={[
-                  username !== ""
-                    ? Styles.filledInputText
-                    : Styles.emptyInputText,
-                  Styles.headlines,
-                ]}
-                className="text-lg w-44  text-center text-slate-600 "
-                onChangeText={setUsername}
-              ></TextInput>
-            </View>
-          </View>
-          <View className="space-y-2 ">
-            <Text
-              style={[Styles.headlines]}
-              className="text-white text-center font-bold text-lg tracking-widest"
-            >
-              Language
-            </Text>
-
-            <View onTouchStart={playFieldSound} className="m-auto">
-              <SelectList
-                data={["English", "Egyptian"]}
-                placeholder="Language"
-                setSelected={setLanguage}
-                search={false}
-                maxHeight={100}
-                boxStyles={[
-                  language !== ""
-                    ? Styles.filledInputText
-                    : Styles.emptyInputText,
-                  {
-                    width: 180,
-                  },
-                ]}
-                inputStyles={[
-                  Styles.headlines,
-                  language !== "" ? { color: "#475569" } : { color: "#0f172a" },
-                ]}
-                dropdownTextStyles={[Styles.inlines, { color: "#475569" }]}
-                dropdownStyles={{ borderRadius: 20 }}
-              />
-            </View>
-          </View>
-          <View className="space-y-2">
-            <Text
-              style={[Styles.headlines]}
-              className="text-white text-center font-bold text-lg tracking-widest"
-            >
-              Players
-            </Text>
-            <View className="m-auto">
-              <TextInput
-                onPressIn={playFieldSound}
-                placeholder="0"
-                placeholderTextColor={"#0f172a"}
-                style={[
-                  players !== ""
-                    ? Styles.filledInputText
-                    : Styles.emptyInputText,
-                  Styles.headlines,
-                ]}
-                className="text-lg w-44 text-center text-slate-600"
-                onChangeText={setPlayers}
-                keyboardType="numeric"
-              ></TextInput>
-            </View>
-          </View>
-
-          <View className="space-y-2">
-            <Text
-              style={[Styles.headlines]}
-              className="text-white text-center font-bold text-lg tracking-widest"
-            >
-              Rounds
-            </Text>
-            <View className="m-auto">
-              <TextInput
-                onPressIn={playFieldSound}
-                placeholder="Max 100"
-                placeholderTextColor={"#0f172a"}
-                style={[
-                  rounds !== ""
-                    ? Styles.filledInputText
-                    : Styles.emptyInputText,
-                  Styles.headlines,
-                ]}
-                className="text-lg w-44 text-center text-slate-600"
-                onChangeText={setRounds}
-                keyboardType="numeric"
-              ></TextInput>
-            </View>
-          </View>
-          <View className="space-y-2">
-            <Text
-              style={[Styles.headlines]}
-              className="text-white text-center font-bold text-lg tracking-widest"
-            >
-              Game Password
-            </Text>
-            <View className="m-auto">
-              <TextInput
-                onPressIn={playFieldSound}
-                placeholderTextColor={"#0f172a"}
-                placeholder="Password"
-                style={[
-                  password !== ""
-                    ? Styles.filledInputText
-                    : Styles.emptyInputText,
-                  Styles.headlines,
-                ]}
-                className="text-lg w-44 text-center text-slate-600"
-                onChangeText={setPassword}
-              ></TextInput>
-            </View>
-          </View>
-        </Animated.View>
-        <Animated.View style={[AS.animatedStylesBtns, AS.opacityStyle]}>
-          <TouchableOpacity
-            //style={{ backgroundColor: "#9F92BD" }}
-            //className="rounded-3xl w-2/4 p-6 mx-auto "
-            onPress={async () => {
-              playSound();
-              await createGame();
-            }}
+              <ArrowLeftCircleIcon color={"#475569"} height={32} width={32} />
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.Text
+            style={[Styles.title, AS.animatedStyles, AS.opacityStyle]}
+            numberOfLines={1}
+            className=" text-center  text-slate-600"
           >
-            <LinearGradient
-              className="rounded-3xl w-2/4 p-6 mx-auto "
-              colors={["#1e3c72", "#2a5298"]}
-            >
+            CREATE GAME
+          </Animated.Text>
+
+          <Animated.View
+            style={[AS.opacityAnimation, AS.animatedStyles]}
+            className="space-y-4 items-center  p-8"
+          >
+            <View className="space-y-2">
               <Text
-                style={Styles.inlines}
-                className="text-white text-md tracking-widest font-bold text-center"
+                style={[Styles.headlines]}
+                className="text-white text-center font-bold text-lg tracking-widest"
               >
-                CREATE
+                Username
               </Text>
-            </LinearGradient>
-          </TouchableOpacity>
-        </Animated.View>
+              <View className="m-auto overflow-x-scroll">
+                <TextInput
+                  onPressIn={playFieldSound}
+                  placeholderTextColor={"#0f172a"}
+                  placeholder="Nickname"
+                  style={[
+                    username !== ""
+                      ? Styles.filledInputText
+                      : Styles.emptyInputText,
+                    Styles.headlines,
+                  ]}
+                  className="text-lg w-44  text-center text-slate-600 "
+                  onChangeText={setUsername}
+                ></TextInput>
+              </View>
+            </View>
+            <View className="space-y-2 ">
+              <Text
+                style={[Styles.headlines]}
+                className="text-white text-center font-bold text-lg tracking-widest"
+              >
+                Language
+              </Text>
+
+              <View onTouchStart={playFieldSound} className="m-auto">
+                <SelectList
+                  data={["English", "Egyptian"]}
+                  placeholder="Language"
+                  setSelected={setLanguage}
+                  search={false}
+                  maxHeight={100}
+                  boxStyles={[
+                    language !== ""
+                      ? Styles.filledInputText
+                      : Styles.emptyInputText,
+                    {
+                      width: 180,
+                    },
+                  ]}
+                  inputStyles={[
+                    Styles.headlines,
+                    language !== ""
+                      ? { color: "#475569" }
+                      : { color: "#0f172a" },
+                  ]}
+                  dropdownTextStyles={[Styles.inlines, { color: "#475569" }]}
+                  dropdownStyles={{ borderRadius: 20 }}
+                />
+              </View>
+            </View>
+            <View className="space-y-2">
+              <Text
+                style={[Styles.headlines]}
+                className="text-white text-center font-bold text-lg tracking-widest"
+              >
+                Players
+              </Text>
+              <View className="m-auto">
+                <TextInput
+                  onPressIn={playFieldSound}
+                  placeholder="0"
+                  placeholderTextColor={"#0f172a"}
+                  style={[
+                    players !== ""
+                      ? Styles.filledInputText
+                      : Styles.emptyInputText,
+                    Styles.headlines,
+                  ]}
+                  className="text-lg w-44 text-center text-slate-600"
+                  onChangeText={setPlayers}
+                  keyboardType="numeric"
+                ></TextInput>
+              </View>
+            </View>
+
+            <View className="space-y-2">
+              <Text
+                style={[Styles.headlines]}
+                className="text-white text-center font-bold text-lg tracking-widest"
+              >
+                Rounds
+              </Text>
+              <View className="m-auto">
+                <TextInput
+                  onPressIn={playFieldSound}
+                  placeholder="Max 100"
+                  placeholderTextColor={"#0f172a"}
+                  style={[
+                    rounds !== ""
+                      ? Styles.filledInputText
+                      : Styles.emptyInputText,
+                    Styles.headlines,
+                  ]}
+                  className="text-lg w-44 text-center text-slate-600"
+                  onChangeText={setRounds}
+                  keyboardType="numeric"
+                ></TextInput>
+              </View>
+            </View>
+            <View className="space-y-2">
+              <Text
+                style={[Styles.headlines]}
+                className="text-white text-center font-bold text-lg tracking-widest"
+              >
+                Game Password
+              </Text>
+              <View className="m-auto">
+                <TextInput
+                  onPressIn={playFieldSound}
+                  placeholderTextColor={"#0f172a"}
+                  placeholder="Password"
+                  style={[
+                    password !== ""
+                      ? Styles.filledInputText
+                      : Styles.emptyInputText,
+                    Styles.headlines,
+                  ]}
+                  className="text-lg w-44 text-center text-slate-600"
+                  onChangeText={setPassword}
+                ></TextInput>
+              </View>
+            </View>
+          </Animated.View>
+          <Animated.View
+            className="space-y-5 "
+            style={[AS.animatedStylesBtns, AS.opacityStyle]}
+          >
+            <TouchableOpacity
+              onPress={async () => {
+                playSound();
+
+                await createGame();
+              }}
+            >
+              <LinearGradient
+                className="rounded-3xl w-2/4 p-6 mx-auto "
+                colors={["#1e3c72", "#2a5298"]}
+              >
+                <Text
+                  style={Styles.inlines}
+                  className="text-white text-md tracking-widest font-bold text-center"
+                >
+                  CREATE
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <View className="">
+              <Text
+                style={[Styles.inlinesForDropDown]}
+                className="text-slate-600 mx-auto"
+              >
+                {errorMsg}
+              </Text>
+            </View>
+          </Animated.View>
+        </ScrollView>
       </SafeAreaView>
     </LinearGradient>
   );
