50914748c1f14112f8334ae98a86e4e0d6b8435a
minaz99 <hanymina43@gmail.com>
2024-03-07 15:19:19 +0100

    feat: added new time ticking sound + added catch for play music sound & added catch for play end of game sound

--- Components/Sounds.js
+++ Components/Sounds.js
@@ -1,23 +1,104 @@
 import { Audio } from "expo-av";
 
 const Sounds = {
-  buttonClickSound: async () => {
+  playButtonClickSound: async () => {
     const { sound } = await Audio.Sound.createAsync(
       require("../assets/audio/ButtonClick.mp3")
     );
     await sound.playAsync();
   },
-  circleClickSound: async () => {
+  playCircleClickSound: async () => {
     const { sound } = await Audio.Sound.createAsync(
       require("../assets/audio/FieldClick.mp3")
     );
     await sound.playAsync();
   },
-  fieldClickSound: async () => {
+  playFieldClickSound: async () => {
     const { sound } = await Audio.Sound.createAsync(
       require("../assets/audio/FieldClick.mp3")
     );
     await sound.playAsync();
+  },
+  playEndOfGameSound: async function playEndOfGameSound(gameMusic, bgMusic) {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/audio/EndOfGame.mp3")
+    );
+    await gameMusic.current
+      .stopAsync()
+      .catch((err) => err)
+      .then(async () => await bgMusic.current.playAsync())
+      .catch((err) => err)
+      .finally(async () => {
+        await sound.playAsync().catch((err) => err);
+      });
+  },
+  playCorrectGuessSound: async function playCorrectGuessSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/audio/CorrectGuess.mp3")
+    );
+
+    await sound.playAsync();
+  },
+  playWrongGuessSound: async function playWrongGuessSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/audio/WrongGuess.mp3")
+    );
+    await sound.playAsync();
+  },
+  playSelectionSound: async function playSelectionSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/audio/OptionSelection.mp3")
+    );
+    await sound.playAsync();
+  },
+  playTimeTickingSound: async function playTimeTickingSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/audio/TimeTicking.mp3")
+    );
+    await sound.playAsync();
+  },
+  playGameMusic: async function playBackgroundMusic(gameMusic, bgMusic) {
+    if (!gameMusic.current._loaded) {
+      await gameMusic.current
+        .loadAsync(require("../assets/audio/MagicInTheAirGameMusic.mp3"))
+        .catch((err) => err)
+        .then(async () => {
+          await bgMusic.current
+            .stopAsync()
+            .catch((err) => err)
+            .then(async () => {
+              await gameMusic.current
+                .playAsync()
+
+                .catch((err) => err)
+                .finally(
+                  async () =>
+                    await gameMusic.current
+                      .setIsLoopingAsync(true)
+                      .catch((err) => err)
+                );
+            });
+        });
+    }
+  },
+  playBackgroundMusic: async function playBackgroundMusic(bgMusic) {
+    if (!bgMusic.current._loaded) {
+      await bgMusic.current
+        .loadAsync(require("../assets/audio/SwingBackgroundMusic.mp3"))
+        .catch((err) => err)
+        .then(
+          async () =>
+            await bgMusic.current
+              .playAsync()
+              .catch((err) => err)
+              .finally(
+                async () =>
+                  await bgMusic.current
+                    .setIsLoopingAsync(true)
+                    .catch((err) => err)
+              )
+        );
+    }
   },
 };
 
