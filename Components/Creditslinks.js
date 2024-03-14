cc38c35a597372d1cb95b52f8fcd469f51645395
minaz99 <hanymina43@gmail.com>
2024-03-15 00:17:02 +0100

    feat: added array of credits objects

---
+++ Components/Creditslinks.js
@@ -0,0 +1,97 @@
+export const CreditsSection = [
+  {
+    link: "https://www.flaticon.com/free-icons/snake",
+    title: "snake icons",
+    text: "Snake icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/ice-cream",
+    title: "ice cream icons",
+    text: "Ice cream icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/comedy",
+    title: "comedy icons",
+    text: "Comedy icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/plane",
+    title: "plane icons",
+    text: "Plane icons created by iconfield - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/gift",
+    title: "gift icons",
+    text: "Gift icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/gelato",
+    title: "gelato icons",
+    text: "Gelato icons created by Kemalmoe - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/lion",
+    title: "lion icons",
+    text: "Lion icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/valentines-day",
+    title: "valentines day icons",
+    text: "Valentines day icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/rain",
+    title: "rain icons",
+    text: "Rain icons created by Umeicon - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/scream",
+    title: "scream icons",
+    text: "Scream icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/snail",
+    title: "snail icons",
+    text: "Snail icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/snowy",
+    title: "snowy icons",
+    text: "Snowy icons created by Superarticons - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/beach-sunset",
+    title: "beach sunset icons",
+    text: "Beach sunset icons created by Aranagraphics - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/sunset",
+    title: "sunset icons",
+    text: "Sunset icons created by logisstudio - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/train",
+    title: "train icons",
+    text: "Train icons created by Vectors Market - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/animal",
+    title: "animal icons",
+    text: "Animal icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/beach",
+    title: "beach icons",
+    text: "Beach icons created by Freepik - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/winter",
+    title: "winter icons",
+    text: "Winter icons created by BomSymbols - Flaticon",
+  },
+  {
+    link: "https://www.flaticon.com/free-icons/tour",
+    title: "tour icons",
+    text: "Tour icons created by gravisio - Flaticon",
+  },
+];
